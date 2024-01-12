885259ba4651c2051fefa07fe2b1a09fc1a99e4c
Gaurav Shrivastav <68452419+gaurav21s@users.noreply.github.com>
2024-01-12 11:15:49 +0530

    Update app.py
    layout change

--- app.py
+++ app.py
@@ -7,16 +7,15 @@ load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function To get response from LLAma 2 model
 os.environ["GOOGLE_API_KEY"] = "AIzaSyDCPEcdPv-N8uEH-oIVrvQdIK4zFxNRjPA"
 
-def getGeminiresponse(input_text,no_words,blog_style):
+def getGeminiresponse(input_text,no_words,blog_style,profession):
 
     model = genai.GenerativeModel('gemini-pro')
 
     template=f"""
         Write a creative blog in {blog_style} style for a topic {input_text}
-        within {no_words} words.
+        within {no_words} words that can help this {profession}.
             """
     
     response = model.generate_content(template,generation_config = genai.types.GenerationConfig(temperature = 0.3))
@@ -25,7 +24,7 @@ def getGeminiresponse(input_text,no_words,blog_style):
     
     
 
-st.set_page_config(page_title="Generate Blogs",
+st.set_page_config(page_title="Your Blog Writing Friend",
                     page_icon='🤖',
                     layout='centered',
                     initial_sidebar_state='collapsed')
@@ -42,13 +41,25 @@ with col1:
     no_words=st.text_input('No of Words')
 with col2:
     blog_style=st.selectbox('Select the blog style',
-                            ('Fun and Saracastic','Depressing','Normal','Intelligent'),index=0)
-    
+                            ('Normal','Fun and Saracasm','Depressing','Intelligent','Motivational'),index=0)
+
+professions = ['Engineer', 'Doctor', 'Teacher', 'Student','Other']
+
+with col1:
+    selected_profession = st.selectbox("What's your profession:", professions)
+
+# If 'Others' is selected, display a text input box
+if selected_profession == 'Other':
+    with col2:
+        profession = st.text_input('Please type your profession:')
+else:
+    profession = selected_profession
+
 submit=st.button("Generate")
 
 ## Final response
 if submit:
-    st.write(getGeminiresponse(input_text,no_words,blog_style))
+    st.write(getGeminiresponse(input_text,no_words,blog_style,profession))
 
 st.markdown("""
     <style>
